dc821b79c69ae433f59c0fef572ab46f4f1789ec
Douaa <douaasekal@gmail.com>
2025-08-07 09:57:30 +0100

    Update the configuration and routes for frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+import { provideMarkdown } from 'ngx-markdown';
+
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+providers: [
+provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+      withFetch() // Add this line
+    ),
+    provideToastr({
+      timeOut: 4000, // 4 seconds
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
+    importProvidersFrom(BrowserAnimationsModule),
+    provideMarkdown(),
+  ]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { LandingLayoutComponent } from './layout/landing-layout/landing-layout.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: 'auth', component: AuthComponent },
+
+{
+path: '',
+component: LandingLayoutComponent,
+children: [
+{ path: '', component: LandingComponent }
+]
+},
+
+{
+path: 'Home',
+component: MainLayoutComponent,
+children: [
+
+]
+},
+
+{ path: '**', redirectTo: '' }
+];
+
